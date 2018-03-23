38541a7293e066b4e9a4400f8cc618fb82417225
Alexandr Voroncov <defauthack@gmail.com>
2018-03-24 01:25:20 +0300

    Add bootstrap template with test JSON-data, add VueJS, show albums on page with tracks

---
+++ js/app.js
@@ -0,0 +1,96 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+  	musicians: [{
+  		"name": "Edward Sheeran",
+  		"age": "27",
+  		"genre": "pop",
+  		"careerStartYear": "2006",
+  		"albums": [{
+  			"name": "Plus",
+  			"releaseYear": "2011",
+  			"tracks": [{
+  				name: "The A Team",
+  				duration: "04:18",
+  				isFavorite: "false",
+  				isListened: "false",
+  				rating: "0"
+  			}, {
+  				name: "Drunk",
+				duration: "03:20",
+				isFavorite: "false",
+				isListened: "false",
+				rating: "0"
+			}, {
+				name: "U.N.I.",
+				duration: "03:48",
+				isFavorite: "false",
+				isListened: "false",
+				rating: "0"
+			}, {
+				name: "Grade 8",
+				duration: "02:59",
+				isFavorite: "false",
+				isListened: "false",
+				rating: "0"
+			}]
+		}, {
+			"name": "Multiply",
+			"releaseYear": "2014",
+			"tracks": [{
+				name: "One",
+				duration: "04:13",
+				isFavorite: "false",
+				isListened: "false",
+				rating: "0"
+			}, {
+				name: "I'm a Mess",
+				duration: "04:06",
+				isFavorite: "false",
+				isListened: "false",
+				rating: "0"
+			}, {
+				name: "Nina",
+				duration: "03:43",
+				isFavorite: "false",
+				isListened: "false",
+				rating: "0"
+			}, {
+				name: "Photograph",
+				duration: "04:17",
+				isFavorite: "false",
+				isListened: "false",
+				rating: "0"
+			}]
+		}, {
+			"name": "Divide",
+			"releaseYear": "2017",
+			"tracks": [{
+				name: "Erazer",
+				duration: "03:47",
+				isFavorite: "false",
+				isListened: "false",
+				rating: "0"
+			}, {
+				name: "Castle on the Hill",
+				duration: "04:21",
+				isFavorite: "false",
+				isListened: "false",
+				rating: "0"
+			}, {
+				name: "Dive",
+				duration: "03:58",
+				isFavorite: "false",
+				isListened: "false",
+				rating: "0"
+			}, {
+				name: "Shape of You",
+				duration: "03:53",
+				isFavorite: "false",
+				isListened: "false",
+				rating: "0"
+			}]
+		}]
+	}]
+  }
+});
