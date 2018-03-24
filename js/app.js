3e7e575e451af30cfe68c201e792a6c468dcb05e
Alexandr Voroncov <defauthack@gmail.com>
2018-03-24 23:53:30 +0300

    Add logic for tracks with like/dislike, isListen, isFavorite

--- js/app.js
+++ js/app.js
@@ -12,27 +12,31 @@ let app = new Vue({
   			"tracks": [{
   				name: "The A Team",
   				duration: "04:18",
-  				isFavorite: "false",
-  				isListened: "false",
-  				rating: "0"
+  				isFavorite: false,
+  				isListened: false,
+  				rating: 0,
+  				isLiked: false
   			}, {
   				name: "Drunk",
 				duration: "03:20",
-				isFavorite: "false",
-				isListened: "false",
-				rating: "0"
+				isFavorite: false,
+				isListened: false,
+				rating: 0,
+				isLiked: false
 			}, {
 				name: "U.N.I.",
 				duration: "03:48",
-				isFavorite: "false",
-				isListened: "false",
-				rating: "0"
+				isFavorite: false,
+				isListened: false,
+				rating: 0,
+				isLiked: false
 			}, {
 				name: "Grade 8",
 				duration: "02:59",
-				isFavorite: "false",
-				isListened: "false",
-				rating: "0"
+				isFavorite: false,
+				isListened: false,
+				rating: 0,
+				isLiked: false
 			}]
 		}, {
 			"name": "Multiply",
@@ -42,25 +46,29 @@ let app = new Vue({
 				duration: "04:13",
 				isFavorite: "false",
 				isListened: "false",
-				rating: "0"
+				rating: 0,
+				isLiked: false
 			}, {
 				name: "I'm a Mess",
 				duration: "04:06",
-				isFavorite: "false",
-				isListened: "false",
-				rating: "0"
+				isFavorite: false,
+				isListened: false,
+				rating: 0,
+				isLiked: false
 			}, {
 				name: "Nina",
 				duration: "03:43",
-				isFavorite: "false",
-				isListened: "false",
-				rating: "0"
+				isFavorite: false,
+				isListened: false,
+				rating: 0,
+				isLiked: false
 			}, {
 				name: "Photograph",
 				duration: "04:17",
-				isFavorite: "false",
-				isListened: "false",
-				rating: "0"
+				isFavorite: false,
+				isListened: false,
+				rating: 0,
+				isLiked: false
 			}]
 		}, {
 			"name": "Divide",
@@ -68,29 +76,59 @@ let app = new Vue({
 			"tracks": [{
 				name: "Erazer",
 				duration: "03:47",
-				isFavorite: "false",
-				isListened: "false",
-				rating: "0"
+				isFavorite: false,
+				isListened: false,
+				rating: 0,
+				isLiked: false
 			}, {
 				name: "Castle on the Hill",
 				duration: "04:21",
-				isFavorite: "false",
-				isListened: "false",
-				rating: "0"
+				isFavorite: false,
+				isListened: false,
+				rating: 0,
+				isLiked: false
 			}, {
 				name: "Dive",
 				duration: "03:58",
-				isFavorite: "false",
-				isListened: "false",
-				rating: "0"
+				isFavorite: false,
+				isListened: false,
+				rating: 0,
+				isLiked: false
 			}, {
 				name: "Shape of You",
 				duration: "03:53",
-				isFavorite: "false",
-				isListened: "false",
-				rating: "0"
+				isFavorite: false,
+				isListened: false,
+				rating: 0,
+				isLiked: false
 			}]
 		}]
 	}]
+  },
+  methods: {
+  	setLike (musicianIndex, albumIndex, tracksIndex) {
+  		this.musicians[musicianIndex].albums[albumIndex].tracks[tracksIndex].isLiked = true;
+  	},
+  	setDislike (musicianIndex, albumIndex, tracksIndex) {
+  		this.musicians[musicianIndex].albums[albumIndex].tracks[tracksIndex].isLiked = false;
+  	},
+  	setListener (musicianIndex, albumIndex, tracksIndex) {
+  		this.musicians[musicianIndex].albums[albumIndex].tracks[tracksIndex].isListened = true;
+  	},
+  	setDislistener (musicianIndex, albumIndex, tracksIndex) {
+  		this.musicians[musicianIndex].albums[albumIndex].tracks[tracksIndex].isListened = false;
+  	},
+  	setFavorite (musicianIndex, albumIndex, tracksIndex) {
+  		this.musicians[musicianIndex].albums[albumIndex].tracks[tracksIndex].isFavorite = true;
+  	},
+  	setDisfavorite (musicianIndex, albumIndex, tracksIndex) {
+  		this.musicians[musicianIndex].albums[albumIndex].tracks[tracksIndex].isFavorite = false;
+  	},
+  	getMusicansFromServer () {
+  		console.log('WOW')
+  	}
+  },
+  created() {
+  	this.getMusicansFromServer();
   }
 });
